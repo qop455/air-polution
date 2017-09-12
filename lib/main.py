c5951859de8920653fa38c49f12771ab6ceaa962
JasonAllyson <jason@JasonAllysonde-MacBook-Pro.local>
2017-09-12 18:31:09 +0800

    Auto create prediction trend chart(6 hours)

--- lib/main.py
+++ lib/main.py
@@ -4,14 +4,17 @@
 """
 
 import pandas as pd
+import numpy as np
 from datetime import datetime
 import matplotlib.pyplot as plt
+import plotly.plotly as py
 import mopy as mo
-
+from sklearn.model_selection import train_test_split
+#plt.rcParams['figure.figsize'] = [16.0, 8.0]
 TIMEFORMAT = "%Y-%m-%d %H:%M:%S"
 
 data = pd.read_csv("../data/central_reg/filted/1.csv")
-data = data[:1000]
+data = data[:100]
 
 for i in range(12):
     data["PM25_"+str(i+1)] = data["PM25"].shift(i+1)
@@ -23,7 +26,7 @@ for i in range(6):
     data["F_WS_"+str(i+1)] = data["WS"].shift(-(i+1))
 for i in range(6):
     data["Target_"+str(i+1)] = data["PM25"].shift(-(i+1))
-
+'''
 timeFeature = pd.DataFrame(index=["hour", "week", "month"])
 for index, item in data.datetime.iteritems():
     time = datetime.strptime(item, TIMEFORMAT)
@@ -37,22 +40,56 @@ month_dummies = pd.get_dummies(data["month"]).rename(columns = lambda x: "m_" + 
 
 data = pd.concat([data, time_dummies, week_dummies, month_dummies], axis=1)
 data.drop(["hour", "week", "month"], inplace=True, axis=1)
-
+'''
 col_PM25 = [f for f in data.columns.tolist() if "PM25" in f]
 col_TEMP = [f for f in data.columns.tolist() if "TEMP" in f]
 col_RH = [f for f in data.columns.tolist() if "RH" in f]
 col_RAIN = [f for f in data.columns.tolist() if "RAIN" in f]
 col_WD = [f for f in data.columns.tolist() if "WD" in f]
 col_WS = [f for f in data.columns.tolist() if "WS" in f]
+col_Target = [f for f in data.columns.tolist() if "Target" in f]
 
-print(data.shape)
+col_F = dict()
+for i in range(6):
+    col_F["F"+str(i+1)] = [f for f in data.columns.tolist() if "F_" in f and "_"+str(i+1) in f]
+
 droplist = list()
-for index, raw in data.iterrows():
-    if mo.CheckSeriesValidity(raw[col_PM25]) and mo.CheckSeriesValidity(raw[col_TEMP]) and mo.CheckSeriesValidity(raw[col_RH]) and mo.CheckSeriesValidity(raw[col_RAIN]) and mo.CheckSeriesValidity(raw[col_WD]) and mo.CheckSeriesValidity(raw[col_WS]):
+for index, row in data.iterrows():
+    if mo.CheckSeriesValidity(row[col_PM25]) and mo.CheckSeriesValidity(row[col_TEMP]) and mo.CheckSeriesValidity(row[col_RH]) and mo.CheckSeriesValidity(row[col_RAIN]) and mo.CheckSeriesValidity(row[col_WD]) and mo.CheckSeriesValidity(row[col_WS]):
         pass
     else:
         droplist.append(index)
 data.drop(data.index[[droplist]], inplace=True)
-data = mo.InterpolateSeries(data)
+data = mo.Interpolate(data)
+train, test = train_test_split(data, test_size = 0.01) 
+ensembleStructure = {"A":"XGB", "B":"MLP", "C":"XGB"}
 
-print(data.shape)
+models = dict()
+features = dict()
+for i in range(6):
+    models["F"+str(i+1)] = mo.EnsembleModel(ensembleStructure)
+    features["F"+str(i+1)] = {"A":col_PM25, "B":col_PM25, "C":col_F["F"+str(i+1)]}
+    models["F"+str(i+1)].fit(train, train["Target_"+str(i+1)], features["F"+str(i+1)])
+
+for index, row in test.iterrows():
+    preds = np.array([])
+    for i in range(6):
+        pred = models["F"+str(i+1)].predict(row, features["F"+str(i+1)])
+        preds = np.append(preds, pred)
+
+    plt.title('6 hours prediction')
+    plt.xlabel('Time Line')
+    plt.ylabel('PM2.5 Value')
+    real = row[col_PM25 + col_Target].tolist()
+    predict = row[col_PM25].tolist() + preds.tolist()
+    plt.plot(range(19), predict, 'r--', label="Predict")
+    plt.plot(range(19), real, 'b-', label="Real")
+    plt.xticks(range(19), [str(x+1) for x in range(19)])
+    plt.xticks(rotation=90)
+    plt.ylim([min(real) - 5, max(real) + 5])
+    plt.axhline(y=15.5, color='g', xmin=0.01, xmax=0.99, alpha=0.8)
+    plt.axhline(y=35.4, color='y', xmin=0.01, xmax=0.99, alpha=0.8)
+    plt.axvline(x=12, color='r', ymin=0.01, ymax=0.99, alpha=0.8)
+    plt.legend()
+    plt.savefig("../output/picture/"+str(index)+".png")
+
--- lib/mopy.py
+++ lib/mopy.py
@@ -4,10 +4,10 @@
 """
 
 from pandas import Series, DataFrame, concat
-from numpy import nan, isnan
+from numpy import nan, isnan, array, append
 from xgboost import XGBRegressor
 from sklearn.neural_network import MLPRegressor
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.metrics import mean_squared_error
 from math import sqrt
 from pickle import dump, load, HIGHEST_PROTOCOL
@@ -29,7 +29,7 @@ def CheckSeriesValidity(series):
     else:
         raise TypeError("Input expected type pandas.Series, received type", type(series))
 
-def InterpolateSeries(data):
+def Interpolate(data):
     if isinstance(data, Series):
         return data.interpolate(limit_direction="both")
     elif isinstance(data, DataFrame):
@@ -46,9 +46,9 @@ class EnsembleModel:
         self.__create()
 
     def __create(self):
-        for k, v in self.structure.iteritems():
+        for k, v in self.structure.items():
             if v == "XGB":
-                self.models[k] = XGBRegressor()
+                self.models[k] = GradientBoostingRegressor()
             elif v == "MLP":
                 self.models[k] = MLPRegressor()
             elif v == "LR":
@@ -62,27 +62,36 @@ class EnsembleModel:
             self.models["B"].fit(x[features["B"]], y)
             
             A_pred = self.models["A"].predict(x[features["A"]])
+            #print("RMSE: %f"%(round(sqrt(mean_squared_error(A_pred, y)), 6)))
             B_pred = self.models["B"].predict(x[features["B"]])
+            #print("RMSE: %f"%(round(sqrt(mean_squared_error(B_pred, y)), 6)))
             
             preds = DataFrame({"A_pred":A_pred, "B_pred":B_pred})
-            C = concat([preds, x[features["C"]]], axis=1)
+            C = x[features["C"]].reset_index(drop=True)
+            C = concat([preds, C], axis=1)
             self.models["C"].fit(C, y)
             
             C_pred = self.models["C"].predict(C)
             self.rmse = round(sqrt(mean_squared_error(C_pred, y)), 6)
-            print("RMSE: %f"%(self.rmse))
+            #print("RMSE: %f"%(self.rmse))
             
         else:
             raise TypeError("Input expected type dict, received type", type(features))
     
     def predict(self, x, features):
-        A_pred = self.models["A"].predict(x[features["A"]])
-        B_pred = self.models["B"].predict(x[features["B"]])
-        
-        preds = DataFrame({"A_pred":A_pred, "B_pred":B_pred})
-        C = concat([preds, x[features["C"]]], axis=1)
-        
-        return self.models["C"].predict(C)
+        if isinstance(x, Series):
+            if isinstance(features, dict): 
+                A = x[features["A"]].values.reshape(1, -1)
+                A_pred = self.models["A"].predict(A)
+                B = x[features["B"]].values.reshape(1, -1)
+                B_pred = self.models["B"].predict(B)
+                C = x[features["C"]].values.reshape(1, -1)
+                C = append(append(A_pred, B_pred), C).reshape(1, -1)
+                return self.models["C"].predict(C)
+            else:
+                raise TypeError("Input expected type dict, received type", type(features))
+        else:
+            raise TypeError("Input expected type pandas.Series, received type", type(x))
     
     def save(self, filename):
         if isinstance(filename, str):
